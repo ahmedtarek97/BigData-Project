88e69577bea0ab9cf84fc7303c7d8729bb55c4b2
ahmedsaied1298 <36535653+ahmedsaeid98@users.noreply.github.com>
2020-05-04 05:55:10 +0200

    Add files via upload

--- support.py
+++ support.py
@@ -1,11 +1,27 @@
-def computeInter(attributes):
-      # TODO
-      trash = 0 # Delete this when begin to work on it.
+from attribute import attribute
+from main import data_frame
+
+def computeInter(list_of_attribute):
+      #in case if there are 2 attribute in the same colum
+      for i in range(len(list_of_attribute)):
+            for j in range(i+1,len(list_of_attribute)-1):
+                  if list_of_attribute[i].col == list_of_attribute[j].col :
+                        return 0
+      
+      sum = 0
+      for i in range(len(data_frame)):
+            flag = 0
+            for j in range(len(list_of_attribute)):
+                  if list_of_attribute[j].val == data_frame[list_of_attribute[j].col][i] : 
+                        flag =1
+                  else :
+                        flag=0
+                        break
+            if flag == 1 :
+                  sum=sum+1
+      return sum
 
 
-
-
-def support(attributes):
-    # TODO
-      trash = 0 # Delete this when begin to work on it.
+def support(list_of_attribute):
+      return ((computeInter(list_of_attribute)/5821 )*100)
 
