84173b26789167d6e779459d6c0d984cd4d6823c
ahmedsaied1298 <36535653+ahmedsaeid98@users.noreply.github.com>
2020-05-06 21:07:00 +0200

    Add files via upload

--- support.py
+++ support.py
@@ -1,5 +1,5 @@
 from attribute import attribute
-from main import data_frame
+from main import DataList
 
 def computeInter(list_of_attribute):
       #in case if there are 2 attribute in the same colum
@@ -7,12 +7,11 @@ def computeInter(list_of_attribute):
             for j in range(i+1,len(list_of_attribute)):
                   if list_of_attribute[i].col == list_of_attribute[j].col :
                         return 0
-      
       sum = 0
-      for i in range(len(data_frame)):
+      for i in range(len(DataList)):
             flag = 0
             for j in range(len(list_of_attribute)):
-                  if list_of_attribute[j].val == data_frame[i][list_of_attribute[j].col] : 
+                  if list_of_attribute[j].val == DataList[i][list_of_attribute[j].col] : 
                         flag =1
                   else :
                         flag=0
@@ -23,5 +22,5 @@ def computeInter(list_of_attribute):
 
 
 def support(list_of_attribute):
-      return ((computeInter(list_of_attribute)/5821 )*100)
+      return ((computeInter(list_of_attribute)/5822 )*100)
 
