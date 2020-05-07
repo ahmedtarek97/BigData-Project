484da3bd7352ce41f9454835c502dc6b10a26c24
ahmed tarek <ahmedtrk97@gmail.com>
2020-05-07 20:53:17 +0200

    added attribute and rule classes

--- BigData.cpp
+++ BigData.cpp
@@ -2,8 +2,44 @@
 
 
 using namespace std;
+class attribute{
+
+public:
+    string name;
+    int col;
+    int val ;
+    attribute(string n , int c , int v)
+    {
+        name = n;
+        col = c;
+        val = v;
+
+    }
+
+};
+
+class rule{
+
+public:
+    vector<attribute> right;
+    vector<attribute> left;
+    rule(vector<attribute>&r, vector<attribute>&l)
+    {
+        right = r;
+        left = l;
+    }
+    double confidence()
+    {
+      vector<attribute>attributes = right.insert( right.end(), left.begin(), left.end() );
+      double num = support(attributes);
+      double den = support(left);
+      double conf = num / den;
+
+      return conf * 100 ;
+    }
 
 
+};
 
 int main(){
 
