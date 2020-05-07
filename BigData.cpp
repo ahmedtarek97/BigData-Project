461f6505c1109d3e2f5d9876eea58e032ee94b7e
omarahmed1111 <omarpirate2010@yahoo.com>
2020-05-07 21:27:50 +0200

    Combine all implemented functions before

--- BigData.cpp
+++ BigData.cpp
@@ -1,7 +1,10 @@
 #include <bits/stdc++.h>
 
-
 using namespace std;
+
+vector<string> ColumnNames = {"Customer","Roman catholic","Protestant","Other religion","No religion",
+"Married","Living together","Other relation","Singles","Household without children","Household with children",
+"High level education"};
 
 class attribute{
 
@@ -18,20 +21,23 @@ public:
     }
 
 };
-
+int computeInter(vector<attribute> list);
+float support(vector<attribute> list);
 class rule{
 
 public:
     vector<attribute> right;
     vector<attribute> left;
-    rule(vector<attribute>&r, vector<attribute>&l)
+    rule(vector<attribute> &r, vector<attribute> &l)
     {
         right = r;
         left = l;
     }
     double confidence()
     {
-      vector<attribute>attributes = right.insert( right.end(), left.begin(), left.end() );
+
+      right.insert( right.end(), left.begin(), left.end() );
+      vector<attribute> attributes = right;
       double num = support(attributes);
       double den = support(left);
       double conf = num / den;
@@ -41,19 +47,56 @@ public:
 
 
 };
+vector<vector<int>> AquireData(){
+	ifstream inputFile;
+	inputFile.open("ticdata2000.txt");
+	vector<vector<int>> DataList(5822,vector<int>(12));
 
-int computeInter(vector<attribute> list);
-float support(vector<attribute> list);
-
-int main(){
-
-
-	
+	for(int i=0;i<5822;i++){
+		vector<int> tmp(49);
+		for(int j=0;j<49;j++){
+			cin>>tmp[j];
+		}
+		for(int j=4;j<16;j++){
+			DataList[i][j-4] = tmp[j];
+		}
+	}
+	return DataList;
 
 
 }
 
-int computeInter(vector<attribute> list)
+vector<attribute> initialize(vector<vector<int>> &DataList){
+	vector<attribute> attributes;
+	map<attribute,bool> vis;
+	for(int i=0;i<12;i++){
+		for(int j=0;j<5822;j++){
+			attribute tmpAttr = attribute(ColumnNames[i],i,DataList[j][i]);
+			if(vis[tmpAttr])continue;
+			vis[tmpAttr] =true;
+			attributes.push_back(tmpAttr);
+		}
+	}
+
+	return attributes;
+}
+
+
+
+int main(){
+
+//	vector<vector<int>> DataList = AquireData();
+//	vector<attribute> attributes = initialize(DataList);
+//	for(int i=0;i<attributes.size();i++){
+//		cout<<attributes[i].name<<" "<<attributes[i].col<<" "<<attributes[i].val<<'\n';
+//	}
+
+
+
+}
+
+
+int computeInter(vector<attribute> list, vector<vector<int>> DataList)
 {
 	//in case of there are 2 element in the same colum
 	for (int i = 0; i < list.size()-1; i++)
@@ -67,12 +110,12 @@ int computeInter(vector<attribute> list)
 		}
 	}
 	int sum = 0;
-	for (int i = 0; i < datalist.size(); i++)
+	for (int i = 0; i < DataList.size(); i++)
 	{
 		int flag = 0;
 		for (int j=0;j< list.size();j++)
 		{
-			if (list[j].val == datalist[i][list[j].col])
+			if (list[j].val == DataList[i][list[j].col])
 			{
 				flag = 1;
 			}
@@ -92,9 +135,7 @@ int computeInter(vector<attribute> list)
 
 float support(vector<attribute> list)
 {
-	return ((computeInter(list) / 5822) * 100);
+	return (((float)computeInter(list) / 5822) * 100);
 }
-
-
 
 
